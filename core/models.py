6d38ef8181b58eb524f5f804a2fd2f2a6caf0f72
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2022-03-31 22:55:29 +0300

    add model user

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+

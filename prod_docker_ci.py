04c74108ef9994d0a2d5605d008ea4ac482824dd
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2022-05-21 21:03:51 +0300

    actions

--- prod_docker_ci.py
+++ prod_docker_ci.py
@@ -1,7 +1,7 @@
-DATABASE_URL = "psql://$DB_USER:$DB_PASSWORD@$DB_HOST/$DB_NAME"
+DATABASE_URL = "psql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME"
 DB_PASSWORD = "$DB_PASSWORD"
 DB_USER = "$DB_USER"
 DB_NAME = "$DB_NAME"
-DB_HOST = "$DB_HOST"
+DB_HOST = "postgres"
 SECRET_KEY = 'django-insecure-8-(s6kcw1h8sv_%@bg+j#(0*zllhq+@o39vgkqfypw=pyhfh8f'
 DEBUG = True

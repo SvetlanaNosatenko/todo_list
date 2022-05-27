b91930b57f69b4fe8aea3e98651bcbe383933d52
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2022-05-28 01:05:42 +0300

    actions

--- prod_docker_ci.py
+++ prod_docker_ci.py
@@ -2,5 +2,5 @@
 POSTGRES_PASSWORD = "${POSTGRES_PASSWORD}"
 POSTGRES_USER = "${POSTGRES_USER}"
 POSTGRES_DB = "${POSTGRES_DB}"
-POSTGRES_HOST = "postgres"
+POSTGRES_HOST = "${POSTGRES_HOST}"
 SECRET_KEY = 'django-insecure-8-(s6kcw1h8sv_%@bg+j#(0*zllhq+@o39vgkqfypw=pyhfh8f'

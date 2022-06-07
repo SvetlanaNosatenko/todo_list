85a918277e183c919c39cbf8837fa3b13f0b7953
Svetlana Nosatenko <s.v.nosatenko@gmail.com>
2022-06-07 21:51:16 +0300

    actions_36

--- todolist/urls.py
+++ todolist/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("bot/", include(("bot.urls", 'bot'), namespace='bot')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
